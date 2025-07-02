abd2a0b84d07a360fa9bc486994c62091c4bd578
Sid-op777 <69779993+Sid-op777@users.noreply.github.com>
2025-07-03 04:40:49 +0530

    fixed test db

--- src/test/java/tech/nx7/vault/VaultApplicationTests.java
+++ src/test/java/tech/nx7/vault/VaultApplicationTests.java
@@ -1,10 +1,22 @@
 package tech.nx7.vault;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import tech.nx7.vault.repository.SnippetRepository;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 class VaultApplicationTests {
+
+	@MockBean
+	private SnippetRepository snippetRepository;
 
 	@Test
 	void contextLoads() {
